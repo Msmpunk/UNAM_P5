a3eec8a6e147b4004187f209f02d42457149717d
msmpunk <msm_010_27_12@hotmail.com>
2018-11-19 23:50:09 -0600

    +Update

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Inico' });
+  res.render('index', { title: 'Inicio' });
+});
+
+router.get('/alumnos', function(req, res, next) {
+  res.render('alumnos', { title: 'Alumnos' });
 });
 
 
